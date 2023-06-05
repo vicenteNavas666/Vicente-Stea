78a9fba649de965a6d1f855e57d503996f23121c
vicenteNavas666 <135588377+vicenteNavas666@users.noreply.github.com>
2023-06-05 08:01:56 +0000

    basic routing + basic nav bar idea

--- steaNewSite1.0/src/app/app-routing.module.ts
+++ steaNewSite1.0/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { Error404Component } from './error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:HomeComponent },
+  {path: 'home', component:HomeComponent },
+  {path: '**', component:Error404Component}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- steaNewSite1.0/src/app/app.module.ts
+++ steaNewSite1.0/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { Error404Component } from './error404/error404.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Error404Component
   ],
   imports: [
     BrowserModule,
